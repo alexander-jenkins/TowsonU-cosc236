3adba327940f2d63c891febf8d627e77d92373ea
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2021-12-12 19:25:51 -0500

    Add files via upload

---
+++ lab2/Lab2Task5.java
@@ -0,0 +1,16 @@
+/*
+Alex Jenkins
+2-4-20
+*/
+public class Lab2Task5{
+    public static void main(String[] args){
+        String firstName = "Alexander";
+        String middleName = "Joseph";
+        String lastName = "Jenkins";
+        char firstInitial = 'A';
+        char middleInitial = 'J';
+        char lastInitial = 'J';
+        System.out.println("My first name is " + firstName + ".\nMy middle name is " + middleName + ".\nMy last name is " + lastName + ".\n");
+        System.out.println("My first initial is " + firstInitial + ".\nMy middle initial is " + middleInitial + ".\nMy last initial is " + lastInitial + ".");
+    }
+}
